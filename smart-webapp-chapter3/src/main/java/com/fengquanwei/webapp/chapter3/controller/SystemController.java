57857813fc9c221879733d71aef399f64c17a1b6
fengquanwei <fengquanwei1996@163.com>
2017-12-11 20:36:31 +0800

    Adjust package name

---
+++ smart-webapp-chapter3/src/main/java/com/fengquanwei/webapp/chapter3/controller/SystemController.java
@@ -0,0 +1,58 @@
+package com.fengquanwei.webapp.chapter3.controller;
+
+import com.fengquanwei.framework.annotation.Action;
+import com.fengquanwei.framework.bean.Param;
+import com.fengquanwei.framework.bean.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 系统管理
+ *
+ * @author fengquanwei
+ * @create 2017/12/11 20:22
+ **/
+public class SystemController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SystemController.class);
+
+    /**
+     * 进入首界面
+     */
+    @Action("get:/")
+    public View index() {
+        return new View("index.jsp");
+    }
+
+    /**
+     * 进入登录界面
+     */
+    @Action("get:/login")
+    public View login() {
+        return new View("login.jsp");
+    }
+
+    /**
+     * 提交登录表单
+     */
+    @Action("post:/login")
+    public View loginSubmit(Param param) {
+        String username = param.getString("username");
+        String password = param.getString("password");
+//        try {
+//            SecurityHelper.login(username, password);
+//        } catch (AuthcException e) {
+//            LOGGER.error("login failure", e);
+//            return new View("/login");
+//        }
+        return new View("/customer");
+    }
+
+    /**
+     * 提交注销请求
+     */
+    @Action("get:/logout")
+    public View logout() {
+//        SecurityHelper.logout();
+        return new View("/");
+    }
+}
